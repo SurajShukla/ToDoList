ed274e767583717bd5f1d8fed4a4245ae9babca3
Arup Rakshit <aruprakshit@rocketmail.com>
2014-12-18 01:05:39 +0530

    Added a route test for task creation.

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -8,4 +8,19 @@ describe 'ToDo App' do
   def app
     ToDoApp.new
   end
+
+  describe "POST /" do
+    it "creates a new task" do
+      post '/', description: "Need to learn meta-programming"
+      expect(last_response.status).to eq 200
+    end
+
+    it "redirects to the home page after creating a new task" do
+      post '/', description: "Need to learn meta-programming"
+      puts last_response.class
+      expect(last_response.redirect?).to be_true
+      follow_redirect!
+      expect(last_request.path).to eq('/')
+    end
+  end
 end
