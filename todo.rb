8aa6213c8823b5cfb15a129920d90d06abd21a60
Arup Rakshit <aruprakshit@rocketmail.com>
2014-12-17 22:00:06 +0530

    Deleted the Task creation lines in Application load time, as now web UI is avaiable for this.

--- todo.rb
+++ todo.rb
@@ -3,9 +3,6 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'sinatra/base'
 require 'task'
 
-Task.create(description: "Create web application")
-Task.create(description: "Profit!")
-
 class ToDoApp < Sinatra::Base
   get "/" do
     @tasks = Task.all
