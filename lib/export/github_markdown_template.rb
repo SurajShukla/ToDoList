4c440c66482fdb1548d22dda3ec4903b64c49b2b
Arup Rakshit <aruprakshit@rocketmail.com>
2014-12-23 02:33:28 +0530

    Spec is added for the class GitHubMarkdownTemplate.

--- lib/export/github_markdown_template.rb
+++ lib/export/github_markdown_template.rb
@@ -1,14 +1,16 @@
 module Export
   class GitHubMarkdownTemplate
-    attr_accessor :document
-
-    def initialize
-      @document = File.read(File.realdirpath('../../views/list.erb', __dir__))
-    end
+    attr_writer :document
 
     def to_markdown(binding)
       template = ERB.new(document, nil, '-')
       template.result(binding)
+    end
+
+    private
+
+    def document
+      @document ||= File.read(File.realdirpath('../../views/list.erb', __dir__))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require_relative '../simple_store.rb'
 require_relative '../todo.rb'
 require_relative '../task.rb'
 require_relative '../list.rb'
+require_relative '../lib/export/github_markdown_template.rb'
 
 RSpec.configure do |config|
   # reset database before each example is run
