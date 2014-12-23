ae75cb9598599cc7be366e5cbbfda893615e003d
Arup Rakshit <aruprakshit@rocketmail.com>
2014-12-23 23:53:01 +0530

    Added the spec for the List class.

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,12 +1,12 @@
 require_relative 'spec_helper.rb'
 
 describe List do
-  let(:task1) { Task.create(description: "Feed the cat") }
-  let(:task2) { Task.create(description: "Feed the cow", done: true) }
-  let(:task3) { Task.create(description: "Feed the dog") }
+  subject { described_class.new([:task]) }
 
-  subject { described_class.new([task1, task2, task3]) }
-
-  describe '#to_markdown' do
+  describe '#to_gist' do
+    it 'converts the list to a Github\'s gist' do
+      expect(Gist).to receive(:gist).with(any_args)
+      subject.to_gist
+    end
   end
 end
