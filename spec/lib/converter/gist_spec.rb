e0a464e2d98dafc40b2ce8b23e3aeafb46ff8648
Arup Rakshit <aruprakshit@rocketmail.com>
2014-12-24 23:56:57 +0530

    issue in spec is fixed.

--- spec/lib/converter/gist_spec.rb
+++ spec/lib/converter/gist_spec.rb
@@ -4,10 +4,13 @@ describe Converter::Gist do
   subject { described_class.new([:a, :b]) }
 
   describe '#convert' do
-    let(:template) { double("Template", :result => 'template') }
+    let(:template) { double("Template") }
+    let(:binding) { double('Binding') }
 
     it 'converts the collection to a GitHub markdown emplate' do
-      allow_any_instance_of(Converter::Gist::ReaderContext).to receive(:get_binding) { binding }
+      allow(subject).to receive(:template).and_return(template)
+      allow(template).to receive(:result).with(binding)
+      expect_any_instance_of(Converter::Gist::ReaderContext).to receive(:get_binding) { binding }
       expect(template).to receive(:result).with(binding)
       subject.convert
     end
